ca989b46c13deba37ccd2a838a1ed6aacf2c9d60
Miracle112 <77540450+Miracle112@users.noreply.github.com>
2021-09-16 13:38:42 +0300

    Add files via upload

---
+++ while28.py
@@ -0,0 +1,12 @@
+f = open('test.txt')
+a = f.read().split()
+a0 = int(a[0])
+a2 = int(a[2])
+a = a[3:]
+result = (a0 / (1 / a2))
+print(result)
+while len(a) > 0:
+    a1 = int(a[1])
+    a = a[2:]
+    result = (result / (1 / a1))
+    print(result)
